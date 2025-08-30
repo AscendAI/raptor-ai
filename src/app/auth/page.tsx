15b7d22f7515872a0aa545dd3f828477266a9f28
islam-ahsanul <ahsanulislam.info@gmail.com>
2025-08-30 16:02:56 +0600

    refactor: remove unused image components from AuthPage

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -12,25 +12,7 @@ export const metadata: Metadata = {
 export default function AuthPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-          priority
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-          priority
-        />
-      </div>
-      <div className="relative container hidden flex-1 shrink-0 items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative container flex-1 shrink-0 items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="text-primary relative hidden h-full flex-col p-10 lg:flex dark:border-r">
           <div className="bg-primary/5 absolute inset-0" />
           <div className="relative z-20 flex items-center text-lg font-medium">
