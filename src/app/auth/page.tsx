79525bad6a37f6965c6585b06c8e3c40c5a59d7d
islam-ahsanul <ahsanulislam.info@gmail.com>
2025-08-30 15:18:45 +0600

    feat: update AuthPage to use Link component for navigation

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { UserAuthForm } from '@/components/auth/user-auth-form';
 
@@ -33,19 +34,21 @@ export default function AuthPage() {
         <div className="text-primary relative hidden h-full flex-col p-10 lg:flex dark:border-r">
           <div className="bg-primary/5 absolute inset-0" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Raptor AI
+            <Link href="/" className="flex items-center cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-6 w-6"
+              >
+                <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+              </svg>
+              Raptor AI
+            </Link>
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="leading-normal text-balance">
